af07eb33b92a94422e4e4eba6e8a487987926377
zaryanz <aryanx404@gmail.com>
2024-07-29 14:16:34 +0530

    Initial Commit

---
+++ MovieFinder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieFinder
+//
+//  Created by Aryan Singh on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Aryan Singh on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
